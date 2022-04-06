a7242e9cbd93a1c9148c02acf7044299df87d75a
sachinsonone99 <sachinsonone09@gmail.com>
2022-04-06 20:33:56 +0530

    rectangle

---
+++ C programming/rectangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int l,b,area;
+printf("enter the length and width of rectangle\n");
+scanf("%d%d",&l,&b);
+area=l*b;
+printf("area of rectangle=%d",area);
+getch();
+}
