59c3d215ede1e4995c7941f7fbcf46dfb395a560
sachinsonone99 <sachinsonone09@gmail.com>
2022-04-06 20:42:31 +0530

    simple interest

---
+++ C programming/simple interest.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float p,r,t,si;
+printf("enter principle,rate and time of interest\n");
+scanf("%f%f%f",&p,&r,&t);
+si=(p*r*t)/100;
+printf("simple interest=%f",si);
+getch();
+}
