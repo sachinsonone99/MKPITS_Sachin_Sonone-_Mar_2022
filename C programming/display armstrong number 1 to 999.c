e00e530f34dacc664445e103d6afbb2f46f7727b
sachinsonone99 <sachinsonone09@gmail.com>
2022-04-28 14:24:23 +0530

    display armstrong number 1 to 999

---
+++ C programming/display armstrong number 1 to 999.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+	int i,i1,i2,n1,n2,n3;
+	i=1;
+	printf("armstrong number\n");
+	while(i<=999)
+	{
+		n1=i%10;
+		n1=i/10;
+		n2=i1%10;
+		n2=i1/10;
+		n3=i2%10;
+		
+	if(i==n3*n3*n3+n2*n2*n2+n1*n1*n1)
+	{
+		printf("i=%d\n");
+		
+	}
+	i++;
+	}
+	
+}
