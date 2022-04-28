cc16ec320b45af0ba4df3e321471bd0edae0191c
sachinsonone99 <sachinsonone09@gmail.com>
2022-04-28 12:25:16 +0530

    while add

---
+++ C programming/while add.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num ;
+printf("enter n digit");
+scanf("%d",&num);
+while(num>0)
+{
+n1=num%10;
+num=num/10;
+add=adda+n1;
+printf("%d",&n1)	
+}
+getch();	
+}
