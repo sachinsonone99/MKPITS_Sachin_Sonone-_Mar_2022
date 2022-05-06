8955bc3cf1ac51233fc49a5b63ca027d5c109310
sachinsonone99 <sachinsonone09@gmail.com>
2022-05-06 07:01:41 +0530

    assig 95

---
+++ C programming/assig 95.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int test();
+void main()
+{
+	test();
+	printf("%d",test(3));
+    printf("\n%d",test(7));
+    printf("\n%d",test(8));
+    printf("\n%d",test(21));
+}   
+    
+    int test(int n)
+         {
+            return n % 10 <= 2 || n % 10 >= 8;
+            getch();
+         }
