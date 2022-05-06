815b9b5baba7efc632f95b9010ae09c83d35b366
sachinsonone99 <sachinsonone09@gmail.com>
2022-05-06 06:52:34 +0530

    ass 92

---
+++ C programming/ass 92.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int test();
+int abs();
+void main()
+{
+	test();
+    printf("%d",test(5, 4));
+    printf("\n%d",test(4, 3));
+    printf("\n%d",test(1, 4));
+}   
+    
+    int test(int x, int y)
+         {
+          return x == 5 || y == 5 || x + y == 5 || abs(x - y) == 5;
+          getch();
+        }
