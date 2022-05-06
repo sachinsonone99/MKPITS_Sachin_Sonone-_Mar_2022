f7edf087d86a77384d718f43af58db539e9db6ce
sachinsonone99 <sachinsonone09@gmail.com>
2022-05-06 06:56:24 +0530

    ass 96

---
+++ C programming/ass 96.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int test();
+void main()
+{
+	test();
+	printf("%d",test(1, 2, 3));
+    printf("\n%d",test(4, 5, 6));
+    printf("\n%d",test(-1, 1, 0));
+    }       
+    int test(int x, int y, int z)
+         {
+           return x == y + z || y == x + z || z == x + y;
+         getch();
+		 }
