453471448c46392546358ed6974eeee3514e841c
sachinsonone99 <sachinsonone@gmail.com>
2022-08-27 11:58:12 +0530

    message

---
+++ radius/radius/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace radius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int radius;
+            Console.WriteLine("enter radius");
+            radius = Convert.ToInt32(Console.ReadLine());
+            float area = 3.14f * radius * radius;
+            Console.WriteLine("area = " + area);
+            float circ = 2 * 3.14f * radius;
+            Console.WriteLine("circ = " + circ);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ rectangle/rectangle/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace rectangle
+{
+    class Project
+    {
+        static void Main(string[] args)
+        {
+            int length, breadth;
+            Console.WriteLine("enter length");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter bredth");
+            breadth = Convert.ToInt32(Console.ReadLine());
+            int area = length * breadth;
+            Console.WriteLine("area = " + area);
+
+            Console.ReadLine();
+        }
+    }
+}
